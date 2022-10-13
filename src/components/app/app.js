aab0ed5fac56475ad63271fc284e62b6fa55197f
SorryLegacy <alexremnev2@gmail.com>
2022-10-13 19:15:13 +0300

    add css

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { withBookstoreService } from "../hoc";
 import { Routes, Route } from "react-router-dom";
-
+import ShopHeader from '../shop-header';
 import {HomePage, CartPage} from "../pages"
+import './app.css';
 
 const App = () => {
 
     return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/cart" element={<CartPage />} />
-        </Routes>)
+        <main role="main" className="container">
+            <ShopHeader numItems={5} total={210}/>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/cart" element={<CartPage />} />
+            </Routes>
+        </main>)
 }
 
 export default withBookstoreService()(App);
--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,44 +1,46 @@
-import React, {Component} from "react";
-import BookListItem from "../book-list-item";
-import { connect } from "react-redux";
-import {withBookstoreService} from "../hoc";
-import { booksLoaded } from "../../actions";
+import React, { Component } from 'react';
+import BookListItem from '../book-list-item';
+import { connect } from 'react-redux';
 
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../../actions';
+
+
+import './book-list.css';
 
 class BookList extends Component {
 
-    componentDidMount() {
-        const { bookstoreService } = this.props;
-        const data = bookstoreService.getBooks();
-        console.log(data)
-        this.props.booksLoaded(data)
+  componentDidMount() {
+    // 1. receive data
+    const { bookstoreService } = this.props;
+    const data = bookstoreService.getBooks();
 
-    }
+    // 2. dispacth action to store
+    this.props.booksLoaded(data);
+  }
 
-    render() {
-        const { books } = this.props
-        return (
-            <ul>
-                {
-                    books.map((book) => {
-                        return (
-                            <li key={book.id}><BookListItem book={book}/></li>
-                        )
-                    })
-                }
-            </ul>
-        )
-    }
+  render() {
+    const { books } = this.props;
+    return (
+      <ul className="book-list">
+        {
+          books.map((book) => {
+            return (
+              <li key={book.id}><BookListItem book={book}/></li>
+            )
+          })
+        }
+      </ul>
+    );
+  }
 }
 
-const mapStateToProps = ({books}) => {
-  return {
-      books
-  }
+const mapStateToProps = ({ books }) => {
+  return { books };
 };
 
 const mapDispatchToProps = {
-    booksLoaded
-}
+  booksLoaded
+};
 
 export default withBookstoreService()(connect(mapStateToProps, mapDispatchToProps)(BookList));
